17e234628b74966a5184e342d029d697d4da8046
William Fink <bladescraft33@gmail.com>
2025-07-21 08:44:02 -0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,21 +1,14 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js';
+import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB8bHVnt00N-H4VBgwsag4oRHBYVvf4q4Q",
-  authDomain: "rep-royale.firebaseapp.com",
-  projectId: "rep-royale",
-  storageBucket: "rep-royale.appspot.com",
-  messagingSenderId: "842201057109",
-  appId: "1:842201057109:web:f2bb869900048bf84751dc",
-  measurementId: "G-JSL641G4SJ"
-};
+const firebaseConfig = { ... };
 
 try {
   const app = initializeApp(firebaseConfig);
   window.auth = getAuth(app);
   window.db = getFirestore(app);
+  window.GoogleAuthProvider = GoogleAuthProvider;
   console.log('Firebase initialized successfully');
 } catch (e) {
   console.error('Firebase initialization failed:', e);
